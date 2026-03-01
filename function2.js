e6ce8ed3786a362605ed77ed198bf7b55e501e13
Renato Amaral Rother <renato.rother@hotmail.com>
2026-03-01 15:44:54 -0300

    Add new functions for adding numbers and displaying names

---
+++ function2.js
@@ -0,0 +1,7 @@
+function addNumbers (num1, num2){
+    console.log(num1 + num2);
+}
+
+let number1 = 100;
+let number2 = 30;
+addNumbers(number1, number2);
---
+++ function3.js
@@ -0,0 +1,9 @@
+function showName (names, index){
+    console.log(names[index])
+}
+
+let names = ['John', 'Paul', 'Ringo', 'George']
+
+for (index = 0; index < names.length; index++) {
+    showName(names, index)
+}
