11327a0985c00f0aebb63f615e5d88763db6dc0f
Renato Amaral Rother <renato.rother@hotmail.com>
2026-08-11 22:23:29 -0300

    11082026

--- RECURSOS/SECCAO+06+-+JAVASCRIPT+PARA+A+WEB+-+DOM/SECCAO 06 - JAVASCRIPT PARA A WEB - DOM/022_mouse_events_parte_2/script.js
+++ RECURSOS/SECCAO+06+-+JAVASCRIPT+PARA+A+WEB+-+DOM/SECCAO 06 - JAVASCRIPT PARA A WEB - DOM/022_mouse_events_parte_2/script.js
@@ -1,32 +1,32 @@
 // mouseover
-document.querySelector("#div1").addEventListener('mouseenter', function(){
+document.querySelector("#div1").addEventListener('mouseenter', function () {
   this.style.backgroundColor = "yellow";
 });
 
 // mousemove
-document.querySelector("#div2").addEventListener('mousemove', function(){
+document.querySelector("#div2").addEventListener('mousemove', function () {
   let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   this.textContent = chars.charAt(Math.floor(Math.random() * chars.length));
 });
 
 // mouseover
-document.querySelector("#div3").addEventListener('mouseover', function(){
+document.querySelector("#div3").addEventListener('mouseover', function () {
   this.style.backgroundColor = "rgb(100,190,255)";
 });
 
 // mouseout
-document.querySelector("#div4").addEventListener('mouseout', function(){
+document.querySelector("#div4").addEventListener('mouseout', function () {
   this.style.backgroundColor = "green";
 });
 
 // contextmenu
-document.querySelector("#div4").addEventListener('contextmenu', function(event){
+document.querySelector("#div4").addEventListener('contextmenu', function (event) {
   event.preventDefault();
   alert('Menu de contexto desativado!');
 });
 
 // wheel
-document.querySelector("#div4").addEventListener('wheel', function(){
+document.querySelector("#div4").addEventListener('wheel', function () {
   let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   this.textContent = chars.charAt(Math.floor(Math.random() * chars.length));
 });
